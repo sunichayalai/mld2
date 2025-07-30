0d4ec693c5d1f9322579c21545f6d113ac8f0ca3
sunichayalai <2401351H@student.tp.edu.sg>
2025-07-31 00:40:56 +0800

    Add files via upload

--- appSpl.py
+++ appSpl.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import joblib
+from imblearn.ensemble import BalancedRandomForestClassifier
 
 model = joblib.load("model_clean.pkl")
 
